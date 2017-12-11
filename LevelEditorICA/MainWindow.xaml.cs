ae227f1a6cea3331fa15922eb281a397d301c561
Michael Thomas <michaelthomas1997n@hotmail.com>
2017-12-11 15:56:19 +0000

    stuff

--- LevelEditorICA/MainWindow.xaml.cs
+++ LevelEditorICA/MainWindow.xaml.cs
@@ -20,9 +20,14 @@ namespace LevelEditorICA
     /// </summary>
     public partial class MainWindow : Window
     {
+
+      
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
 
         private void ExitEditorCommand(object sender, RoutedEventArgs e)

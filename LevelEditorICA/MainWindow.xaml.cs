bdd409e25a078e248d8d48c3762d5a6b7ecf512e
Michael Thomas <michaelthomas1997n@hotmail.com>
2017-12-15 00:42:41 +0000

    Changed listbox to canvas

--- LevelEditorICA/MainWindow.xaml.cs
+++ LevelEditorICA/MainWindow.xaml.cs
@@ -34,9 +34,9 @@ namespace LevelEditorICA
             #region IMAGES
             //////////////////////////////////////////////////////////////
             // initialise the source of tiles
-            SpriteSheetList.ItemsSource = panelImages;
+            //SpriteSheetList.ItemsSource = panelImages;
             // initialise the source of map's tiles
-            SpriteSheetList.ItemsSource = mapImages;
+            //SpriteSheetList.ItemsSource = mapImages;
             //////////////////////////////////////////////////////////////
             #endregion
 
@@ -53,7 +53,7 @@ namespace LevelEditorICA
                     rect_.Width = tileSize;
                     rect_.Height = tileSize;
                     rect_.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-                    mapTileCanvas.Children.Add(rect_);
+                    //mapTileCanvas.Children.Add(rect_);
                     Canvas.SetLeft(rect_, tileSize * j + j * 1);
                     Canvas.SetTop(rect_, tileSize * i + i * 1);
 
@@ -77,22 +77,29 @@ namespace LevelEditorICA
 
         // size, in pixels, of tiles considered
         // (shouldnt this be stored in the config file?!)
-        int tileSize = 32;
+        private int tileSize = 32;
 
-        int gridHeight = 5;
-        int gridWidth = 5;
+        private int gridHeight = 5;
+        private int gridWidth = 5;
+
+        List<Image> images = new List<Image>();
 
         List<Rectangle> tiles = new List<Rectangle>();
 
+       
+
         private Rectangle rect = new Rectangle();
- 
+
+        Image GetImg = new Image();
+
         // this is a special type of collection which automatically updates the UI element it is bound to 
         ObservableCollection<Image> panelImages = new ObservableCollection<Image>();
         ObservableCollection<Image> mapImages = new ObservableCollection<Image>();
-
+        Collection<Image> collectionImages = new Collection<Image>();
+        
         // drag and drop
-        //Point mouseOffset;
-        //private bool isDragging = false;
+        Point mouseOffset;
+        private bool isDragging = false;
 
         //private int _boundNumber;
         public int BoundNumber
@@ -142,14 +149,15 @@ namespace LevelEditorICA
                             Button btn = new Button();
 
                             Canvas can = new Canvas();
-
+                            Grid grid = new Grid();
                             Rectangle rect_ = new Rectangle();
                             rect_.Width = tileSize;
                             rect_.Height = tileSize;
                             rect_.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-                            mapTileCanvas.Children.Add(rect_);
+                            //mapTileCanvas.Children.Add(rect_);
                             Canvas.SetLeft(rect_, tileSize * j + j * 1);
                             Canvas.SetTop(rect_, tileSize * i + i * 1);
+
                             
 
 
@@ -188,11 +196,11 @@ namespace LevelEditorICA
                             rect_.Width = tileSize;
                             rect_.Height = tileSize;
                             rect_.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-                            mapTileCanvas.Children.Add(rect_);
+                            //mapTileCanvas.Children.Add(rect_);
                             Canvas.SetLeft(rect_, tileSize * j + j * 1);
                             Canvas.SetTop(rect_, tileSize * i + i * 1);
-
-
+                            
+                            
 
 
 
@@ -217,6 +225,9 @@ namespace LevelEditorICA
                 
             }
         }
+
+        
+        
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName = null)
@@ -295,7 +306,7 @@ namespace LevelEditorICA
 
             if (result.HasValue && result.Value)
             {
-
+                   
                 string sFilenames = "";
 
                 foreach (string sFilename in openFileDialog.FileNames)
@@ -307,7 +318,29 @@ namespace LevelEditorICA
 
         }
 
-        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+
+
+        private void Mouse_overCanvas(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition((UIElement) sender);
+
+            
+
+            //Rectangle rect_ = new Rectangle();
+            //rect_.Width = tileSize;
+            //rect_.Height = tileSize;
+            //rect_.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            //SpriteSheetList.Children.RemoveAt(0);
+            //SpriteSheetList.Children.Insert(1, rect);
+            Image newImg = new Image();
+            //mapTileCanvas.Children.Add(new Image() { Source = ((Image)newImg).Source, Height = tileSize});
+            
+            //images.Insert(1, newImg);
+
+            //mapTileCanvas.Children.Add()
+            
+        }
+            private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
             //openFileDialog.InitialDirectory = "c://";
@@ -328,28 +361,93 @@ namespace LevelEditorICA
                 }
                 sFilenames = sFilenames.Substring(1);
 
-
+                
                 BitmapImage bitmap = new BitmapImage(new Uri(sFilenames,UriKind.Relative));
+
+                SpriteSheetList.Height = bitmap.PixelHeight;
+                SpriteSheetList.Width = bitmap.PixelWidth;
+
 
                 string filename = openFileDialog.FileName;
                 for (int i = 0; i < bitmap.PixelHeight / tileSize; i++)
                     for (int j = 0; j < bitmap.PixelWidth / tileSize; j++)
                     {
-                        panelImages.Add(new Image
+                        Image newImg = new Image()
                         {
+                           
                             Source = new CroppedBitmap(bitmap,
                                                  new Int32Rect(j * tileSize, i * tileSize, tileSize, tileSize)),
                             Height = tileSize
-                    });
+                        };
+                       
+                        SpriteSheetList.Children.Add(newImg);
+                        Canvas.SetTop(newImg, i * tileSize);
+                        Canvas.SetLeft(newImg, j * tileSize); 
+                        images.Add(newImg);
+                        //newImg.MouseLeftButtonDown += Image_MouseDown;
 
+                        GetImg = newImg;
+                        
                         
                         //panelImages.Last().MouseLeftButtonDown += Image_MouseDown;
+                        collectionImages.Add(newImg);
                     }
-                SpriteSheetList.ItemsSource = new ObservableCollection<Image>(panelImages);
-                
+                //SpriteSheetList.ItemsSource = new ObservableCollection<Image>(panelImages);
                
+                
+
             }
 
+        }
+
+       
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Dragging mode begins
+            isDragging = true;
+            Image img = (Image)sender;
+
+            // Get the position of the click relative to the image
+            // so the top-left corner of the image is (0,0)
+            mouseOffset = e.GetPosition(img);
+
+            // Watch this image for more mouse events
+            img.MouseMove += Image_MouseMove;
+            img.MouseLeftButtonUp += Image_MouseUp;
+
+            // Capture the mouse. This way you'll keep receiving
+            // the MouseMove event even if the user jerks the mouse
+            // off the image
+            img.CaptureMouse();
+        }
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Image img = (Image)sender;
+
+                // Get the position of the image relative to the Canvas
+                Point point = e.GetPosition(SpriteSheetList);
+
+                img.SetValue(Canvas.TopProperty, point.Y - mouseOffset.Y);
+                img.SetValue(Canvas.LeftProperty, point.X - mouseOffset.X);
+            }
+        }
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isDragging)
+            {
+                Image img = (Image)sender;
+
+                // Don't watch the mouse events any longer
+                img.MouseMove -= Image_MouseMove;
+                img.MouseLeftButtonUp -= Image_MouseUp;
+                img.ReleaseMouseCapture();
+
+                isDragging = false;
+            }
         }
 
         private void ExitEditorCommand(object sender, RoutedEventArgs e)
@@ -358,13 +456,14 @@ namespace LevelEditorICA
         }
 
         // add the selected tile to the map
-        private void btn_AddTileToMap(object sender, RoutedEventArgs e)
-        {
-            if (SpriteSheetList.SelectedItem != null)
-            {
-                mapImages.Add(new Image() { Source = ((Image)SpriteSheetList.SelectedItem).Source, Height = tileSize });
-            }
-        }
+        //private void btn_AddTileToMap(object sender, RoutedEventArgs e)
+        //{
+        //    if (SpriteSheetList.SelectedItem != null)
+        //    {
+        //        mapImages.Add(new Image() { Source = ((Image)SpriteSheetList.SelectedItem).Source, Height = tileSize });
+                
+        //    }
+        //}
 
         
     }
